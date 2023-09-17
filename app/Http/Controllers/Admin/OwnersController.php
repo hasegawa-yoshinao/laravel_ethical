d71ef42b5572ecaa78f6b65997f4d9f7e44361f0
hasegawa-yoshinao <yoshinao.4470.hasegawa@gmail.com>
2023-09-18 07:07:14 +0900

    shopCreateDelete

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -5,9 +5,12 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Owner; //Eloquent
+use App\Models\Shop; //Eloquent
 use Illuminate\Support\Facades\DB;//queryBuilder
-use Illuminate\Support\Facades\Hash;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
+use Throwable;
+use Illuminate\Support\Facades\Log;
 
 class OwnersController extends Controller
 {
@@ -64,11 +67,27 @@ class OwnersController extends Controller
             'password' => 'required|string|confirmed|min:8',
         ]);
 
-        Owner::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+        try{
+            DB::transaction(function () use($request) {
+                $owner = Owner::create([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'password' => Hash::make($request->password),
+                ]);
+
+                Shop::create([
+                    'owner_id' => $owner->id,
+                    'name' => '店名を入力してください',
+                    'information' => '',
+                    'filename' => '',
+                    'is_selling' => true
+
+                ]);
+            },2);
+        }catch(Throwable $e){
+            Log::error($e);
+            throw $e;
+        }
 
         return redirect()
         ->route('admin.owners.index')
@@ -96,8 +115,7 @@ class OwnersController extends Controller
      */
     public function edit($id)
     {
-        $owner = Owner::findOrFail($id);
-        // dd($owner);
+        $owner = Owner::with('shop')->findOrFail($id);
         return view('admin.owners.edit', compact('owner'));
     }
 
